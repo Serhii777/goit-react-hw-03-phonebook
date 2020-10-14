a53fb706cecc045f3e0e6aa740715b7681c5bd19
Serhii777 <muzykasv72@gmail.com>
2020-10-14 19:51:39 +0300

    Updates

---
+++ precache-manifest.4602fa72b5bec2de489e3cce2340c9a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7723bf02ec750951753580747c5b1ee",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "a89a781152afebbf40c9",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.361613aa.chunk.css"
+  },
+  {
+    "revision": "7973b23e69736aa0087c",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.acc2525e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.acc2525e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a89a781152afebbf40c9",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.20a43ff6.chunk.js"
+  },
+  {
+    "revision": "159f9e7c304b75423aa5",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.cca09c81.js"
+  }
+]);
